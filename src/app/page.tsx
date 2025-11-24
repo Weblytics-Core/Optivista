508d35a2a7076707fd4f5ac9b51c58b378556268
subhadotcom <117196507+subhadotcom@users.noreply.github.com>
2025-11-24 22:39:37 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@
   import { ContactForm } from '@/components/contact-form';
   import { processImageUrl } from '@/lib/utils';
   import { WatermarkedImage } from '@/components/watermarked-image';
-  import { getAdminFirestore } from '@/firebase/server';
 
   export const metadata: Metadata = {
     title: 'Optivista',
